10f7720af88a881237015b17b6a99fa128491de0
IdaB00 <ida.berg@student.careeria.fi>
2026-06-01 15:11:23 +0300

    modelit ja index luotu

---
+++ app/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Pelin genre   
+class Genre(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+# Peli
+class Game(models.Model):
+    gametitle = models.CharField(max_length=200, default="game")
+    description = models.TextField()
+    releaseyear = models.PositiveIntegerField()
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    cover = models.ImageField(upload_to="covers/", blank=True, null=True)
+
+    def __str__(self):
+        return self.gametitle
+    
+# Pelikirjasto  
+class GameLibrary(models.Model):
+    STATUS_CHOICES = [
+        ("owned", "Owned"),
+        ("playing", "Playing"),
+        ("completed", "Completed"),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    added = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="owned")
