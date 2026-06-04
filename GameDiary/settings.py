26d53aebed65fbe2d7ceb9a721ff196eea970b63
IdaB00 <ida.berg@student.careeria.fi>
2026-06-04 12:26:48 +0300

    Debug asetus false

--- GameDiary/settings.py
+++ GameDiary/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(6)&i^@+x34toyf(0g&$^4d%t86!^5+m11eg02j@l3=q=%$o!n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
